a77cb37f2712894a8ca6c9b4ed63bb6991b1bc4a
Kaat <bolgovn92@gmail.com>
2020-07-06 17:52:51 +0300

    work with vectors

--- src/culculutur.c
+++ src/culculutur.c
@@ -1,157 +1,95 @@
-/*
- ============================================================================
- Name        : culculutur.c
- Author      : Cat
- Version     :
- Copyright   : mya
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
-
 #include <stdio.h>
 #include <stdlib.h>
-
-float factorial(int n)  //function for calculating the factorial(n-number)
-{
-    float res;
-    int i;
-    res = 1;
-    i = 1;
-    while (i != (n + 1)) {
-        res = res * i;
-        i++;
-    }
-    return (res);
-}
-
-float stepen(float x, float y) //function for calculating the degree(x-number, y-degree)
-{
-    float res;
-    int i;
-    res = 1;
-    i = 0;
-    while (i < y) {
-        res = res * x;
-        i++;
-    }
-    if (y < 0) {
-        res = 1 / res;
-    }
-    return (res);
-}
-
-int main(int argc, char *argv[]) {
+int size , mode;
+float first_var, second_var , result , g;
+float *vec1 , *vec2;
+char operation, next_time;
+int main(int argc , char *argv[] ) {
     setvbuf(stdout, NULL, _IONBF, 0);
     setvbuf(stderr, NULL, _IONBF, 0);
-    float *vector1, *vector2, a, b, result;
-    int size;
-    char choose, operation, inputfile[100], outputfile[100], repeat = 'y';
-    FILE *input, *output;
-
-    while (repeat == 'y') { // Cycle to repeat the program.
-        printf("Enter input file name:\n "); // Enter the name of the input and output file.
-        scanf("%s", inputfile);
-        printf("Enter output file name:\n ");
-        scanf("%s", outputfile);
-        input = fopen(inputfile, "r"); // Opening input and output files.
-        output = fopen(outputfile, "w");
-        while (feof(input) == 0) { // Loop to read the file to the end.
-            fscanf(input, " %c", &choose); // Choose what to work with: numbers or vectors.
-            fscanf(input, " %c", &operation); // Selecting an operation.
-            switch (choose) {
-            case 's': // Choosing to work with numbers.
-                fscanf(input, " %f", &a);
-                fscanf(input, " %f", &b);
-                switch (operation) { // Selecting an operation for numbers.
-                case '+': // Addition.
-                    fprintf(output, "%.2f + %.2f = %.2f", a, b, a + b);
-                    break;
-                case '-': // Difference.
-                    fprintf(output, "%.2f - %.2f = %.2f", a, b, a - b);
-                    break;
-                case '*': // Multiplication.
-                    fprintf(output, "%.2f * %.2f = %.2f", a, b, a * b);
-                    break;
-                case '/': // Division.
-                    if (b != 0)
-                        fprintf(output, "%.2f / %.2f = %.2f", a, b, a / b);
-                    else
-                        fprintf(output, "No solution");
-                    break;
-                case '^': // Exponentiation.
-                    fprintf(output, "%.2f ^ %.2f = %.2f", a, b, stepen(a, b));
-                    break;
-                case '!': // Factorial calculation.
-                    fscanf(input, "%f", &a);
-                    if (a >= 0) {
-                        fprintf(output, "%.0f! = %.0f", a, factorial(a));
-                    } else
-                        fprintf(output, "This operation is not valid");
-                    break;
+    next_time = 'y';
+    while (next_time == 'y'){ // Цикл на повторение программы неколько раз
+       printf("Selected operating mode(1- numbers , 2 - vectors)");
+       scanf("%i", &mode);
+       if (mode == 1){
+            puts("enter the first var ");
+            scanf("%f", &first_var);
+            puts("enter your operation  ");
+            scanf(" %c", &operation);
+            g = first_var ;
+            if (operation == '+') { // Сложение двух чисел
+                puts("enter the second  var ");
+                scanf("%f", &second_var);
+                result = first_var + second_var ;
+            }else if (operation == '-') {   // Разность
+                puts("enter the second  var ");
+                scanf("%f", &second_var);
+                result = first_var - second_var ;
+            }else if (operation == '*') {    // Произведение
+                puts("enter the second  var ");
+                scanf("%f", &second_var);
+                result = first_var * second_var ;
+            }else if (operation == '/') {   // Деление
+                puts("enter the second  var ");
+                scanf("%f", &second_var);
+                result = first_var / second_var ;
+            }else if (operation == '^') {   // Возведение в степень
+                puts("enter the second  var ");
+                scanf("%f", &second_var);
+                while (second_var >1){
+                    first_var = first_var * g;
+                    second_var = second_var - 1;
+                   }
+                result = first_var ;
+            }else if (operation == '!') {    // Вычисление факториала
+                while (g > 1 ){
+                    g = g - 1;
+                    first_var = first_var *g ;
                 }
-                break;
-            case 'v': // Choosing to work with vectors.
-                fscanf(input, "%i", &size); // Entering the size of the vectors.
-                vector1 = malloc(size * sizeof(float));
-                vector2 = malloc(size * sizeof(float));
-
-                for (int i = 0; i < size; i++) // Entering the coordinates of the vectors.
-                    fscanf(input, "%f", &vector1[i]);
-                for (int i = 0; i < size; i++)
-                    fscanf(input, "%f", &vector2[i]);
-
-                switch (operation) { // Selecting an operation for vectors.
-                case '+': // Addition.
-                    fprintf(output, "( ");
-                    for (int i = 0; i < size; i++)
-                        fprintf(output, "%.2f ", vector1[i]);
-                    fprintf(output, " ) + ( ");
-                    for (int i = 0; i < size; i++)
-                        fprintf(output, "%.2f ", vector2[i]);
-                    fprintf(output, " ) = ( ");
-                    for (int i = 0; i < size; i++) {
-                        fprintf(output, "%.2f ", vector1[i] + vector2[i]);
-                    }
-                    fprintf(output, " )");
-                    break;
-                case '-': // Difference.
-                    fprintf(output, "( ");
-                    for (int i = 0; i < size; i++)
-                        fprintf(output, "%.2f ", vector1[i]);
-                    fprintf(output, " ) - ( ");
-                    for (int i = 0; i < size; i++)
-                        fprintf(output, "%.2f ", vector2[i]);
-                    fprintf(output, " ) = ( ");
-                    for (int i = 0; i < size; i++) {
-                        fprintf(output, "%.2f ", vector1[i] - vector2[i]);
-                    }
-                    fprintf(output, " )");
-                    break;
-                case '*': // Scalar product.
-                    fprintf(output, "( ");
-                    for (int i = 0; i < size; i++)
-                        fprintf(output, "%.2f ", vector1[i]);
-                    fprintf(output, " ) * ( ");
-                    for (int i = 0; i < size; i++)
-                        fprintf(output, "%.2f ", vector2[i]);
-                    fprintf(output, " ) = ");
-                    for (int i = 0; i < size; i++) {
-                        result += vector1[i] * vector2[i];
-                    }
-                    fprintf(output, "%.2f ", result);
-                    break;
-                }
-                free(vector1); // Free memory.
-                free(vector2);
+                result = first_var;
+            }
+            result = first_var;
+            printf("%f" , result );    // Выводим результат
+            puts(" repeat it? (enter \"y\" to continue)"); // Спрашиваем о перезапуске программы
+            scanf(" %c", &next_time);
+        }
+      else if(mode == 2){
+            printf("Enter the size of the vectors: ");
+            scanf("%i", &size);
+            vec1 = calloc(size,sizeof(float));
+            for (int i=0; i<size; i++){
+                printf("Enter the first vector: ");
+                scanf("%f", &vec1[i]); }
+            vec2 = calloc(size,sizeof(float));
+            for (int i=0; i<size; i++){
+                printf("Enter the second vector: ");
+                scanf("%f", &vec2[i]); }
+            printf("Select the sign(+-*):");
+            scanf(" %c", &operation);
+            switch(operation){
+            case '+':
+               for (int i = 0; i < size; i++){
+               printf("%f", vec1[i] + vec2[i]);}
+               break;
+            case '-':
+               for (int i = 0; i < size; i++){
+               printf("%f", vec1[i] - vec2[i]);}
+               break;
+            case '*':
+                for (int i = 0; i < size; i++){
+                result += vec1[i] * vec2[i];}
+                printf("Result: %f\n", result);
                 break;
             }
-            fprintf(output, "\n");
+            printf("%f" , result );
+            puts(" repeat it? (enter \"y\" to continue)"); // Спрашиваем о перезапуске программы
+            scanf(" %c", &next_time);
         }
-
-        fclose(input); // Closing input and output files.
-        fclose(output);
-        repeat = 0;
-        printf("Do you want to continue?" " " "y-Yes/n-No \n"); // Choosing what to do next.
-        scanf(" %c", &repeat);
+        printf("%f" , result );    // Выводим результат
+        free (vec1);
+        free (vec2);
+        puts(" repeat it? (enter \"y\" to continue)"); // Спрашиваем о перезапуске программы
+        scanf(" %c", &next_time);
     }
+    return 0;
 }
